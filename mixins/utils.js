b46fcf982f35bd6e1caf2bcbd43684eea6ca83b8
Haj_aun <hau.nguyen145207@gmail.com>
2020-11-09 23:25:05 +0700

    update

--- mixins/utils.js
+++ mixins/utils.js
@@ -10,18 +10,18 @@ export default {
 
     // Gets the mouse position
     getMousePos: (e) => {
-      // let posx = 0
-      // let posy = 0
-      // if (!e) e = window.event
-      // if (e.pageX || e.pageY) {
-      //   posx = e.pageX
-      //   posy = e.pageY
-      // } else if (e.clientX || e.clientY) {
-      //   posx = e.clientX + body.scrollLeft + document.documentElement.scrollLeft
-      //   posy = e.clientY + body.scrollTop + document.documentElement.scrollTop
-      // }
+      let posx = 0
+      let posy = 0
+      if (!e) e = window.event
+      if (e.pageX || e.pageY) {
+        posx = e.pageX
+        posy = e.pageY
+      } else if (e.clientX || e.clientY) {
+        posx = e.clientX + body.scrollLeft + document.documentElement.scrollLeft
+        posy = e.clientY + body.scrollTop + document.documentElement.scrollTop
+      }
 
-      // return { x: posx, y: posy }
+      return { x: posx, y: posy }
     },
 
     // Linear interpolation
